629e1100f8c686ed29c8a0d623985db1f11f92cb
Puneet017 <95585582+Puneet017@users.noreply.github.com>
2022-01-09 22:43:30 +0530

    created +venumber
    this program will display positive numbers from a given list.

---
+++ +venumber.py
@@ -0,0 +1,16 @@
+print("->list 1 is [12,-7,5,64,-14].")
+
+list1=[12,-7,5,64,-14]
+ 
+print("=>Positive numbers in list 1 are:")
+for num in list1:
+     if num>=0:
+         print(num,end=",")
+
+print("\n ->list 2 is [12,14,-95,3].")
+
+list2=[12,14,-95,3]
+print("=>Positive numbers in list 2 are:")
+for num in list2:
+    if num>=0:
+     print(num,end=",")
